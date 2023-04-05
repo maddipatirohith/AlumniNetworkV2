fbf184d83325f24930a41f976a29ee85497e099a
maddipatirohith <109060247+maddipatirohith@users.noreply.github.com>
2023-04-05 13:41:05 +0530

    changed db URL

--- db.js
+++ db.js
@@ -2,13 +2,13 @@ const mongoose = require("mongoose");
 
 function connectToMongo() {
     mongoose.set("strictQuery", true);
-    mongoose.connect('mongodb://127.0.0.1:27017/CG',
+    /*mongoose.connect('mongodb://127.0.0.1:27017/CG',
     {useNewUrlParser: true,
     useUnifiedTopology: true,
     useCreateIndex: true,
     useFindAndModify: false}
-);
-    /* mongoose.connect('mongodb+srv://SyedFahad:qVSWAthZ3bgrzha@cluster0.q71wutp.mongodb.net/AlumniNetwork?retryWrites=true&w=majority',
+);*/
+     mongoose.connect('mongodb+srv://SyedFahad:qVSWAthZ3bgrzha@cluster0.q71wutp.mongodb.net/AlumniNetwork?retryWrites=true&w=majority',
         {
             useNewUrlParser: true,
             useUnifiedTopology: true,
@@ -16,7 +16,7 @@ function connectToMongo() {
             useFindAndModify: false
         }
     );
-     */
+     
 
     const db = mongoose.connection;
 
